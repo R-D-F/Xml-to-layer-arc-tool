6ec002ad9c9895a85df0f698dd2a53250ae357f4
Riley Francis <rifrancis22@gmail.com>
2024-04-04 18:44:42 -0700

    very little

--- scratch.py
+++ scratch.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as et
 import pprint
+import json
 
 xml = "ref/ignacio_mare_island_2_115kv_true.xml"
 tree = et.parse(xml)
@@ -44,10 +45,16 @@ def get_xml_tables(xml_file):
 tables = {branch.get('tagname'): branch for branch in root if
             branch.tag == 'table' and int(branch.get('nrows'))}
 structure_dict = xml_table_element_dict(
-            tables['structure_coordinates_report'],
+            tables['construction_staking_report'],
             as_list=False)
+to_remove = {i for i in structure_dict if
+                structure_dict[i]['stake_description'] != 'Structure Hub'}
+for i in to_remove:
+    structure_dict.pop(i)
 
-
-print(structure_dict)
-
-print("\n".join("{}\t{}".format(k, v) for k, v in structure_dict.items()))
+r_dict = {}
+for cnt, i in enumerate(sorted(structure_dict)):
+    r_dict[cnt] = {}
+    rec = structure_dict[i]
+    print(rec['structure_comment_1'])
+# print(json.dumps(structure_dict, indent=4))
